393a43ba2da39b68152f4dea1cd71f10b4c919d3
kamiya555 <kamiya.aka@gmail.com>
2015-10-25 12:04:03 +0900

    Undo changes

--- niconico.js
+++ niconico.js
@@ -1,18 +1,18 @@
 module.exports.seiga = function(args, content) {
   var id = args[0];
-  return '<div><iframe width="312" height="176" src="//ext.seiga.nicovideo.jp/thumb/' + id + '" scrolling="no" style="border:solid 1px #888;" frameborder="0"><a href="//seiga.nicovideo.jp/seiga/' + id + '"></a></iframe></div>';
+  return '<div><iframe width="312" height="176" src="http://ext.seiga.nicovideo.jp/thumb/' + id + '" scrolling="no" style="border:solid 1px #888;" frameborder="0"><a href="http://seiga.nicovideo.jp/seiga/' + id + '"></a></iframe></div>';
 };
 
 module.exports.douga = function (args, content) {
   var id = args[0],
       thumb = args[1] === 'thumb' ? true : false;
   if (thumb) {
-    return '<div><iframe width="312" height="176" src="//ext.nicovideo.jp/thumb/' + id + '" scrolling="no" style="border:solid 1px #CCC;" frameborder="0"></iframe></div>';
+    return '<div><iframe width="312" height="176" src="http://ext.nicovideo.jp/thumb/' + id + '" scrolling="no" style="border:solid 1px #CCC;" frameborder="0"></iframe></div>';
   }
-  return '<div><script type="text/javascript" src="//ext.nicovideo.jp/thumb_watch/' + id + '"></script></div>';
+  return '<div><script type="text/javascript" src="http://ext.nicovideo.jp/thumb_watch/' + id + '"></script></div>';
 }
 
 module.exports.nama = function(args, content) {
   var id = args[0];
-  return '<div><iframe width="312" height="176" src="//live.nicovideo.jp/embed/' + id + '" scrolling="no" style="border:solid 1px #CCC;" frameborder="0"><a href="//live.nicovideo.jp/watch/' + id + '"></a></iframe></div>';
+  return '<div><iframe width="312" height="176" src="http://live.nicovideo.jp/embed/' + id + '" scrolling="no" style="border:solid 1px #CCC;" frameborder="0"><a href="http://live.nicovideo.jp/watch/' + id + '"></a></iframe></div>';
 };
